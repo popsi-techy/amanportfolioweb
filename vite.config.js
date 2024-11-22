5638445cb57078d31e0677f6e2241521aaec5d2e
popsi-techy <amanlearn13@gmail.com>
2024-11-22 13:44:51 +0530

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: " https://popsi-techy.github.io/amanportfolioweb/",
   plugins: [react()],
+  base: "/amanportfolioweb/",
 })
